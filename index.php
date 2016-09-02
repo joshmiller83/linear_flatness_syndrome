88ec58961c0cb715383c4adb77efa1bd06e54ed4
Josh Miller <josh@commerceguys.com>
2016-09-01 21:35:51 -0400

    Isolating the Presentation

--- index.php
+++ index.php
@@ -3,27 +3,13 @@
 $link = new PDO("mysql:host=localhost;dbname=blog_db", 'root');
 
 $result = $link->query('SELECT id, title FROM post');
-?>
 
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>List of Posts</title>
-  </head>
-  <body>
-  <h1>List of Posts</h1>
-  <ul>
-    <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
-      <li>
-        <a href="/show.php?id=<?= $row['id'] ?>">
-          <?= $row['title'] ?>
-        </a>
-      </li>
-    <?php endwhile ?>
-  </ul>
-  </body>
-  </html>
+$posts = array();
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+  $posts[] = $row;
+}
 
-<?php
 $link = null;
-?>
+
+// include the HTML presentation code
+require 'templates/list.php';
